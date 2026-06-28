d677d763b510f8ec61f2ec130b9e47d8ecfa9ef5
Arsh-Tyagi <230502270+adtyagi-git@users.noreply.github.com>
2026-06-28 23:24:20 +0530

    Time: 339 ms (16.02%), Space: 228.4 MB (7.55%) - LeetHub

--- 2150-find-all-lonely-numbers-in-the-array/2150-find-all-lonely-numbers-in-the-array.cpp
+++ 2150-find-all-lonely-numbers-in-the-array/2150-find-all-lonely-numbers-in-the-array.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     vector<int> findLonely(vector<int>& nums) {
-        vector<int>hsh(1e6+2,0);
+        unordered_map<int,int>hsh;
         vector<int> ans;
         for(int i:nums) hsh[i]++;
         for(int i:nums){
-            if(hsh[i]==1 && (i==0||hsh[i-1]==0) && hsh[i+1]==0) ans.push_back(i);
+            if(hsh[i]==1 && hsh[i-1]==0 && hsh[i+1]==0) ans.push_back(i);
         } 
         return ans;
     }
