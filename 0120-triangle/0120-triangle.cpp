a0de0c984d6121a5d96bf65c421019e15c3f24ff
Arsh-Tyagi <230502270+adtyagi-git@users.noreply.github.com>
2026-07-13 11:57:30 +0530

    Time: 0 ms (100%), Space: 12.7 MB (84.04%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -11,18 +11,32 @@ class Solution {
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
         int n=triangle.size();
-        vector<vector<int>> dp(n,vector<int>(n,-1));
-        // return solve(0,0,n,triangle,dp);
+        // vector<vector<int>> dp(n,vector<int>(n,-1));
+        // // return solve(0,0,n,triangle,dp);
+        // for(int j=0;j<n;j++){
+        //     dp[n-1][j]=triangle[n-1][j];
+        // }
+        // for(int i=n-2;i>=0;i--){
+        //     for(int j=i;j>=0;j--){
+        //         int down=triangle[i][j]+dp[i+1][j];
+        //         int diagonal=triangle[i][j]+dp[i+1][j+1];
+        //         dp[i][j]=min(down,diagonal);
+        //     }
+        // }
+        // return dp[0][0];
+        vector<int> prev(n,-1);
+        vector<int>curr(n,-1);
         for(int j=0;j<n;j++){
-            dp[n-1][j]=triangle[n-1][j];
+            prev[j]=triangle[n-1][j];
         }
         for(int i=n-2;i>=0;i--){
             for(int j=i;j>=0;j--){
-                int down=triangle[i][j]+dp[i+1][j];
-                int diagonal=triangle[i][j]+dp[i+1][j+1];
-                dp[i][j]=min(down,diagonal);
+                int down=triangle[i][j]+prev[j];
+                int diagonal=triangle[i][j]+prev[j+1];
+                curr[j]=min(down,diagonal);
             }
+            prev=curr;
         }
-        return dp[0][0];
+        return prev[0];
     }
 };
