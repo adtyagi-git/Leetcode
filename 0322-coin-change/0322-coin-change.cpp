7fcdc059de6f1c48f3210dfa244961947f707ca6
Arsh-Tyagi <230502270+adtyagi-git@users.noreply.github.com>
2026-07-15 14:19:41 +0530

    Time: 47 ms (34.69%), Space: 49 MB (36.37%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -13,11 +13,31 @@ private:
         int exclude=solve(i-1,amount,dp,coins);
         return dp[i][amount]=min(include,exclude);
     }
+    int solve2(int i,int amount,vector<int>& coins){
+        int n=coins.size();
+        vector<vector<int>>dp(n,vector<int>(amount+1,1e9));
+        for(int i=0;i<amount+1;i++){
+            if(i%coins[0]==0) dp[0][i]=i/coins[0];
+        }
+        for(int i=0;i<n;i++){
+            dp[i][0]=0;
+        }
+        for(int i=1;i<n;i++){
+            for(int j=1;j<amount+1;j++){
+                int include=1e9;
+                if(coins[i]<=j) include=1+dp[i][j-coins[i]];
+                int exclude=dp[i-1][j];
+                dp[i][j]=min(include,exclude);
+            }
+        }
+        return dp[n-1][amount]==1e9?-1:dp[n-1][amount];
+    }
 public:
     int coinChange(vector<int>& coins, int amount) {
         int n=coins.size();
-        vector<vector<int>> dp(n,vector<int>(amount+1,-1));
-        int ans=solve(n-1,amount,dp,coins);
-        return (ans>=1e9)?-1:ans;
+        // vector<vector<int>> dp(n,vector<int>(amount+1,-1));
+        // int ans=solve(n-1,amount,dp,coins);
+        // return (ans>=1e9)?-1:ans;
+        return solve2(n-1,amount,coins);
     }
 };
