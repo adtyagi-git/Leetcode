c0e4e525dd155eb7ae8066bb8f498753cf60bf91
Arsh-Tyagi <230502270+adtyagi-git@users.noreply.github.com>
2026-08-01 21:59:01 +0530

    Time: 0 ms (100%), Space: 10.1 MB (52.91%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -2,7 +2,7 @@ class Solution {
 private:
 int solve(vector<int>&nums,vector<vector<int>>&dp,int l,int r){
     if(l==r) return nums[l];
-    if(dp[l][r]==-1) return dp[l][r];
+    if(dp[l][r]!=-1) return dp[l][r];
     int pickleft=nums[l]-solve(nums,dp,l+1,r);
     int pickright=nums[r]-solve(nums,dp,l,r-1);
     return dp[l][r]=max(pickleft,pickright);
@@ -11,7 +11,7 @@ int solve(vector<int>&nums,vector<vector<int>>&dp,int l,int r){
 public:
     bool predictTheWinner(vector<int>& nums) {
         int n=nums.size();
-        vector<vector<int>>dp(n,vector<int>(n,0));
+        vector<vector<int>>dp(n,vector<int>(n,-1));
         return solve(nums,dp,0,n-1)>=0;
     }
 };
