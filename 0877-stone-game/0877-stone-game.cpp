83d660038d064cb28bdd78333782a1881c525a74
Arsh-Tyagi <230502270+adtyagi-git@users.noreply.github.com>
2026-08-02 18:27:30 +0530

    Time: 0 ms (100%), Space: 10.4 MB (68.24%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -3,19 +3,20 @@ private:
 
 public:
     bool stoneGame(vector<int>& piles) {
-        int n=piles.size();
-        int l=0,r=n-1;
-        vector<vector<int>>dp(n,vector<int>(n,0));
-        for(int i=0;i<n;i++){
-            dp[i][i]=piles[i];
-        }
-        for(int l=n-1;l>=0;l--){
-            for(int r=l+1;r<n;r++){
-                int pickleft=piles[l]+dp[l+1][r];
-                int pickright=piles[r]+dp[l][r-1];
-                dp[l][r]=max(pickleft,pickright);
-            }
-        }
-        return dp[0][n-1]>=0;
+    //     int n=piles.size();
+    //     int l=0,r=n-1;
+    //     vector<vector<int>>dp(n,vector<int>(n,0));
+    //     for(int i=0;i<n;i++){
+    //         dp[i][i]=piles[i];
+    //     }
+    //     for(int l=n-1;l>=0;l--){
+    //         for(int r=l+1;r<n;r++){
+    //             int pickleft=piles[l]+dp[l+1][r];
+    //             int pickright=piles[r]+dp[l][r-1];
+    //             dp[l][r]=max(pickleft,pickright);
+    //         }
+    //     }
+    //     return dp[0][n-1]>=0;
+    return true;
     }
 };
