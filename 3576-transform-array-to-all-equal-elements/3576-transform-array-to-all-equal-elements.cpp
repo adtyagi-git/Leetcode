55d53676b279eea4e767090b5f716a5821352423
Arsh-Tyagi <230502270+adtyagi-git@users.noreply.github.com>
2026-08-17 17:01:43 +0530

    Time: 9 ms (71.13%), Space: 163.2 MB (47.92%) - LeetHub

---
+++ 3576-transform-array-to-all-equal-elements/3576-transform-array-to-all-equal-elements.cpp
@@ -0,0 +1,20 @@
+class Solution {
+private:
+    bool check(vector<int> nums, int k, int tar) {
+        int n = nums.size(), count = 0;
+
+        for (int i = 0; i < n - 1; i++) {
+            if (nums[i] != tar) {
+                nums[i] *= -1;
+                nums[i + 1] *= -1;
+                count++;
+                if (count > k) return false;
+            }
+        }
+        return nums[n - 1] == tar;
+    }
+public:
+    bool canMakeEqual(vector<int>& nums, int k) {
+        return check(nums, k, 1) || check(nums, k, -1);
+    }
+};
